777b2bd9b4166ff8e32d4dc0c8026284f00bbf30
saydyhanovssamir-ux <saydyhanovssamir@gmail.com>
2026-09-24 19:13:53 +0300

    Create hitbox.lua

---
+++ hitbox.lua
@@ -0,0 +1,3 @@
+-- Script taken from https://xenoscripts.com website --
+
+loadstring(game:HttpGet('https://gist.githubusercontent.com/Platzhere/39f5435adeb1a744b96aae3dc5cd622c/raw/9f99ea75a99ed54a391ece7445942fd02b1ad83d/Hitbox'))()
